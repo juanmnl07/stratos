cfbd74c9d9c93f0d202362223c52d01704907518
Andrey <andreys2009@gmail.com>
2014-11-11 17:00:47 -0600

    Estilo Servicios Empresarios

---
+++ sites/all/themes/stratos_theme/js/global.js
@@ -0,0 +1,15 @@
+jQuery.noConflict(); 
+jQuery(document).ready(function(){
+	
+	var IE = '';
+	if(jQuery.browser.msie == true){
+		if(jQuery.browser.version == '8.0' || jQuery.browser.version == '7.0'){
+			IE = true;
+		}
+		
+	}
+	var box = jQuery("div[class^=roll-over]"); 
+	box.show().animate({"transform": "scale(0,0)"});
+
+
+});
